eccb9142b28d45bb5fa4597157247a5a37fbdede
Danny Sanchez <dannysanchez559@gmail.com>
2021-05-12 02:11:52 -0700

    add audio sounds to button clicks

--- js/main.js
+++ js/main.js
@@ -6,26 +6,37 @@ let randomInt;
 const picAmount = 20; // constant for amount of 8ball pics in images
 const picNameStart = "./img/magic8ball_"; // file location + start of file name
 
+// AUDIO ELEMENTS
+const onSound = document.getElementById("onSound");
+const offSound = document.getElementById("offSound");
+const shakeSound = document.getElementById("shake");
+
 // DISABLE SHAKE BUTTON BY DEFAULT
 toggleShakeButton();
 
 // ON / OFF BUTTON PRESS
 onButton.addEventListener('click', function() {
-    // TURN ON 8 BALL
-    if (!onSwitch) {
-        onButton.innerHTML = "OFF";
-        onButton.classList.add("turnOffButtonBg");
 
-        // change 8ball image to show On mode
-        ballImage.src = picNameStart + "extra.png";
+    // TURN OFF 8 BALL
+    if (onSwitch) {
+        // play sound off audio
+        play("off");
 
-        // TURN OFF 8 BALL
-    } else if (onSwitch) {
         onButton.innerHTML = "ON";
         onButton.classList.remove("turnOffButtonBg");
 
-        // change 8ball img to show Off mode
+        // change 8ball image to show On mode
         ballImage.src = picNameStart + "start.png";
+
+        // TURN ON 8 BALL
+    } else {
+        // play sound on audio
+        play("on");
+        onButton.innerHTML = "OFF";
+        onButton.classList.add("turnOffButtonBg");
+
+        // change 8ball img to show Off mode
+        ballImage.src = picNameStart + "extra.png";
     };
 
     onSwitch = !onSwitch;
@@ -34,6 +45,8 @@ onButton.addEventListener('click', function() {
 
 // SHAKE BUTTON PRESS - HANDLE IMG SWAP AND ANIMATION
 shakeButton.addEventListener('click', function () {
+    // play shake audio
+    play("shake");
 
     // disable shake button as soon as it's clicked
     toggleShakeButton();
@@ -60,6 +73,28 @@ function getRandomNumber() {
 
 // ENABLE/DISABLE SHAKE BUTTON
 function toggleShakeButton() {
-    shakeButton.classList.toggle("buttonDisabled");
+    shakeButton.classList.toggle("shakeButtonDisabled");
     shakeButton.disabled = !shakeButton.disabled;
+}
+
+// PLAY AUDIO SOUNDS FUNCTION
+function play(soundType) {
+    
+    switch (soundType) {
+        case "on":
+            onSound.play();
+            break;
+
+        case "off":
+            offSound.play();
+            break;
+
+        case "shake":
+            shakeSound.play();
+            break;
+
+        default:
+            break;
+    };
+
 }
